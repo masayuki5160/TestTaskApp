f77b537f37488fb60ff3bec7426588bbc013e296
Masayuki Tanaka <masayuki5160@gmail.com>
2020-03-31 16:06:28 +0900

    タスクを配列に保存する処理を追加

--- TestTaskApp/ViewController.swift
+++ TestTaskApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    private var taskList: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,19 +30,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let alertController = UIAlertController(title: "タスク追加", message: "追加するタスクを入力してください", preferredStyle: .alert)
         
         // アラートダイアログに追加するOKボタン
-        // OKボタンをタップした時の処理はクロージャで実装
-        let oKAlertAction = UIAlertAction(title: "OK", style: .default) {[weak alertController] (action) in
-            if let inputText = alertController?.textFields?[0].text, inputText.count > 0 {
-                print("入力あり：\(inputText)")
-            } else {
-                print("入力なし")
+        let oKAlertAction = UIAlertAction(title: "OK", style: .default) {[weak self] (action) in
+            // OKボタンをタップした時の処理
+            if let inputText = alertController.textFields?.first?.text, inputText.count > 0 {
+                self?.taskList.append(inputText)
+                self?.tableView.reloadData()
             }
         }
         // アラートダイアログに追加するキャンセルボタン
         let cancelAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
         
         // 各パーツをUIAlertControllerに追加
-        alertController.addTextField(configurationHandler: nil)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "タスク詳細"
+        }
         alertController.addAction(oKAlertAction)
         alertController.addAction(cancelAlertAction)
         
@@ -52,22 +54,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // UITableViewDataSourceのdelegateメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // 仮実装としてTableViewに表示するセルの数を5としています (セクションの数はデフォルト1となっています)
-        return 5
+        // TableViewに表示するセルの数
+        return taskList.count
     }
 
     // UITableViewDataSourceのdelegateメソッド.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 作成したオリジナルのTableViewCellを利用
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as! TaskTableViewCell
+        cell.taskName.text = taskList[indexPath.row]
+
         return cell
     }
     
     // UITableViewDataSourceのdelegateメソッド. セルをタップした時の処理.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let alertController = UIAlertController(title: "タスク完了", message: "タスクを完了しますか？", preferredStyle: .alert)
-        let okAlertAction = UIAlertAction(title: "完了", style: .default) { (action) in
-            print("タスク完了")
+        let okAlertAction = UIAlertAction(title: "完了", style: .default) { [weak self] (action) in
+            self?.taskList.remove(at: indexPath.row)
+            self?.tableView.reloadData()
         }
         let cancelAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
         
